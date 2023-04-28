a9c40b1449ea907e8c3d25135f269fe7f6e7a014
c9aw <131928559+c9aw@users.noreply.github.com>
2023-04-28 14:56:09 -0400

    Update HowItWorks.jsx

--- src/components/About/HowItWorks.jsx
+++ src/components/About/HowItWorks.jsx
@@ -3,19 +3,19 @@ const data = [
     id: 1,
     image: '/placeholder-image.jpg',
     title:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore assumenda beatae.',
+      'Our team hand picks new or lightly used items from high-demand brands. All items pass a verification process to ensure each and every one is legit and authentic.',
   },
   {
     id: 2,
     image: '/placeholder-image.jpg',
     title:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore assumenda beatae.',
+      'Items arrive to us are either/and cleaned, restored, ironed, washed, and stored in a dust-free and temperature controlled space.',
   },
   {
     id: 3,
     image: '/placeholder-image.jpg',
     title:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore assumenda beatae.',
+      'We list our products on the eBay platform at the best pricing so you can save even on the most popular brands. We quickly ship our item out to you with sustainable packaging, tracking, and quick shipping.',
   },
 ]
 
