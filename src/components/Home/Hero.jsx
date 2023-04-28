2ea8ff6e6ab66a610fd579544ad5fec7457c72ec
c9aw <131928559+c9aw@users.noreply.github.com>
2023-04-28 13:51:31 -0400

    Update Hero.jsx

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -5,8 +5,8 @@ const Hero = () => {
       <div className='text-white h-full w-full absolute bg-black opacity-80 flex flex-col items-center justify-center'>
         <img className='w-[700px] sm:w-[300px]' src='sellution.png' alt='' />
         <p className='w-[600px] sm:w-[300px] text-center sm:text-sm'>
-          The best place to save on your favorite brands and help the
-          environment at the same time.
+          #1 place to buy, sell, trade, and save on 
+          new and lightly used clothing from your favorite brands.
         </p>
         <div className='flex gap-4 p-4 justify-center text-sm'>
           <button className='border-[3px] w-[7rem] border-white py-1 '>
