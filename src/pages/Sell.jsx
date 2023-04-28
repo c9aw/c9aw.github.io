005c4400189bc4627536ecdbb7ee6390cadab8a6
c9aw <131928559+c9aw@users.noreply.github.com>
2023-04-28 16:57:06 -0400

    Update Sell.jsx

--- src/pages/Sell.jsx
+++ src/pages/Sell.jsx
@@ -1,6 +1,6 @@
 const Sell = () => {
   return (
-    <div className="flex justify-center items-center h-screen text-2xl font-bold tracking-widest animate-pulse">Comming Soon...</div>
+    <div className="flex justify-center items-center h-screen text-2xl font-bold tracking-widest animate-pulse">Coming Soon...</div>
   )
 }
 
