03bd4708c6d478444e4156c5aca00266be3b8453
c9aw <131928559+c9aw@users.noreply.github.com>
2023-04-29 19:32:25 -0400

    Update TOF.jsx

--- src/pages/TOF.jsx
+++ src/pages/TOF.jsx
@@ -9,18 +9,43 @@ const TOF = () => {
         </p>
         <p>Our Terms and Conditions were last updated on September 12, 2022.</p>
         <p>
-          Welcome to sellution.shop. The sellution.shop website (the "Site") is comprised of various web pages operated by Sellution LLC (" Sellution "). sellution.shop is offered to you conditioned on your acceptance without modification of the terms, conditions, and notices contained herein (the "Terms"). Your use of sellution.shop constitutes your agreement to all such Terms. Please read these terms carefully, and keep a copy of them for your reference.
+          The terms of this agreement ("Terms of Service") govern the
+          relationship between you and Beluga, a game development studio on
+          “Roblox”, (hereinafter "Beluga" or "Us" or "We") regarding your use of
+          Beluga's games, store, sites and related services (the "Service").
+          Before accessing or using the Service, including browsing any Beluga
+          website or accessing a game, you must agree to these Terms of Service
+          and the Privacy Policy. By using or registering for an Account or
+          otherwise using the Service, you affirm that you are the legal age of
+          majority in your country of residence. If you are not, your legal
+          guardian must review and agree to these Terms of Service.
         </p>
         <p>
-          sellution shop is an E-Commerce Site. Resale of used and new name-brand clothing for both the buyer to save money and to lower our carbon footprint.
+          BY PLAYING, USING OR OTHERWISE ACCESSING THE SERVICE, YOU AGREE TO
+          THESE Terms of Service. IF YOU DO NOT AGREE TO THESE Terms of Service,
+          PLEASE DO NOT PLAY, USE OR OTHERWISE ACCESS THE SERVICE. USE OF THE
+          SERVICE IS VOID WHERE PROHIBITED.
         </p>
-        <p className='text-black text-3xl'>1. Electronic Communications</p>
         <p>
-         Visiting sellution.shop or sending emails to Sellution constitutes electronic communications. You consent to receive electronic communications and you agree that all agreements, notices, disclosures and other communications that we provide to you electronically, via email and on the Site, satisfy any legal requirement that such communications be in writing.
+          Beluga reserves the right, at its discretion, to change, modify, add
+          or remove portions of these Terms of Service, and other relevant
+          Beluga policies at any time by posting the amended terms on the
+          Service. You will be deemed to have accepted such changes by
+          continuing to use the Service. If at any point you do not agree to any
+          portion of the then-current version of our Terms of Service, or any
+          other Beluga policy, rules or codes of conduct relating to your use of
+          the Service, your right to use the Service shall immediately
+          terminate, and you must immediately stop using the Service.
         </p>
-        <p className='text-black text-3xl'>1. Your Account</p>
+        <p className='text-black text-3xl'>1. Acknowledgment</p>
         <p>
-          If you use this site, you are responsible for maintaining the confidentiality of your account and password and for restricting access to your computer, and you agree to accept responsibility for all activities that occur under your account or password. You may not assign or otherwise transfer your account to any other person or entity. You acknowledge that Sellution is not responsible for third party access to your account that results from theft or misappropriation of your account. Sellution and its associates reserve the right to refuse or cancel service, terminate accounts, or remove or edit content in our sole discretion.
+          Subject to your agreement and continuing compliance with these Terms
+          of Service and any other relevant Beluga policies, you have a
+          non-exclusive, non-transferable, non-sublicensable, revocable and
+          limited right to access and use the Service for your own
+          non-commercial entertainment purposes. You agree not to use the
+          Service for any other purpose. The following restrictions apply to the
+          use of the Service:
         </p>
         <ul className='list-disc ml-10'>
           <li>
@@ -41,22 +66,133 @@ const TOF = () => {
             e-mail or repetitive or misleading messages to anyone.
           </li>
         </ul>
-        <p className='text-black text-3xl'>2. Users Under Thirteen</p>
+        <p className='text-black text-3xl'>2. Use Limitations</p>
         <p>
-          Sellution does not knowingly collect, either online or offline, personal information from persons under the age of thirteen. If you are under 18, you may use sellution.shop only with permission of a parent or guardian.
+          Use Limitations Any use of the Service in violation of these Use
+          Limitations is strictly prohibited, can result in the immediate
+          revocation of your limited right and may subject you to liability for
+          violations of law.
         </p>
-        <p className='text-black text-3xl'>2.1 Links to Third Party Sites/Third Party Services</p>
+        <p>You agree that you will not, under any circumstances:</p>
+        <ul className='list-disc ml-10'>
+          <li>
+            Participate in any act that Beluga determines to be in violation of
+            the Terms or improper use of Beluga's services.
+          </li>
+          <li>
+            Use or participate in the use of cheats, hacks, exploits, mods, or
+            any unauthorized third-party software used to interfere or modify
+            any Beluga game.
+          </li>
+          <li>
+            The interference of other user's normal experience flow in a
+            negatively-impacting way in one of Beluga's game. This includes
+            intentionally abusing a bug in-game to your advantage or to other's
+            disadvantage, promising players with "deals" you won't complete,
+            harassing users into giving you something.
+          </li>
+          <li>
+            Attempting to or being able to access unreleased content or
+            information, developer/testing accounts, unobtainable items/content,
+            admin panel, testing-related content.
+          </li>
+          <li>
+            Post any information that contains nudity, excessive violence, or
+            offensive subject matter or that contains a link to such content.
+          </li>
+          <li>
+            Attempt to, or harass, abuse, or harm, or advocate or incite
+            harassment, abuse, or harm of another person, group, including
+            Beluga employees.
+          </li>
+          <li>
+            Make available through the Service any material or information that
+            infringes any copyright, trademark, patent, trade secret, right of
+            privacy, right of publicity, or other right of any person or entity
+            or impersonates any other person, including without limitation a
+            Beluga employee.
+          </li>
+          <li>
+            Solicit or attempt to solicit Login Information or any other login
+            credentials or personal information from other users of the Service
+            or any Beluga game. Collect or post anyone's private information,
+            including personally identifiable information (whether in text,
+            image or video form), identification documents, or financial
+            information through the Service. Use any Beluga game for gambling,
+            betting or any similar activity in which prizes or rewards can be
+            won (directly or indirectly), including betting on the outcome of
+            matches in which you participate as a player, irrespective of
+            whether or not there is a fee or stake involved.
+          </li>
+        </ul>
+        <p className='text-black text-3xl'>2.1 Limitation Use Violations</p>
         <p>
-          sellution.shop may contain links to other websites ("Linked Sites"). The Linked Sites are not under the control of Sellution and Sellution is not responsible for the contents of any Linked Site, including without limitation any link contained in a Linked Site, or any changes or updates to a Linked Site. Sellution is providing these links to you only as a convenience, and the inclusion of any link does not imply endorsement by Sellution of the site or any association with its operators. Certain services made available via sellution.shop are delivered by third party sites and organizations. By using any product, service or functionality originating from the sellution.shop domain, you hereby acknowledge and consent that Sellution may share such information and data with any third party with whom Sellution has a contractual relationship to provide the requested product, service or functionality on behalf of sellution.shop users and customers.
+          Beluga reserves the right to determine what conduct is considered to
+          be in violation of the rules of use or otherwise outside the intent or
+          spirit of these Terms of Service or the Service itself. Beluga
+          reserves the right to take action as a result, which may include
+          terminating your Account and prohibiting you from using the Service in
+          whole or in part.
         </p>
-        <p className='text-black text-3xl'>3. No Unlawful or Prohibited Use/Intellectual Property</p>
+        <p className='text-black text-3xl'>3. Copyright Policy</p>
+        <p className='text-black text-3xl'>
+          3.1 Intellectual Property Infringement
         </p>
-        You are granted a non-exclusive, non-transferable, revocable license to access and use sellution.shop strictly in accordance with these terms of use. As a condition of your use of the Site, you warrant to Sellution that you will not use the Site for any purpose that is unlawful or prohibited by these Terms. You may not use the Site in any manner which could damage, disable, overburden, or impair the Site or interfere with any other party's use and enjoyment of the Site. You may not obtain or attempt to obtain any materials or information through any means not intentionally made available or provided for through the Site.
+        We respect the intellectual property rights of others. It is Our policy
+        to respond to any claim that
         <p>
-          All content included as part of the Service, such as text, graphics, logos, images, as well as the compilation thereof, and any software used on the Site, is the property of Sellution or its suppliers and protected by copyright and other laws that protect intellectual property and proprietary rights. You agree to observe and abide by all copyright and other proprietary notices, legends or other restrictions contained in any such content and will not make any changes thereto.
+          Content posted on the Service infringes a copyright or other
+          intellectual property infringement of any person.
         </p>
         <p>
-          You will not modify, publish, transmit, reverse engineer, participate in the transfer or sale, create derivative works, or in any way exploit any of the content, in whole or in part, found on the Site. Sellution content is not for resale. Your use of the Site does not entitle you to make any unauthorized use of any protected content, and in particular you will not delete or alter any proprietary rights or attribution notices in any content. You will use protected content solely for your personal use, and will make no other use of the content without the express written permission of Sellution and the copyright owner. You agree that you do not acquire any ownership rights in any protected content. We do not grant you any licenses, express or implied, to the intellectual property of Sellution or our licensors except as expressly authorized by these Terms.
+          If you are a copyright owner, or authorized on behalf of one, and you
+          believe that the copyrighted work has been copied in a way that
+          constitutes copyright infringement that is taking place through the
+          Service, you must submit your notice in writing to the attention of
+          our team via email (team@belugagames.io) and include in your notice a
+          detailed description of the alleged infringement.
+        </p>
+        <p>
+          You may be held accountable for damages for misrepresenting that any
+          Content is infringing your copyright.
+        </p>
+        <p className='text-black text-3xl'>3.1.1 Intellectual Property</p>
+        <p>
+          The Service and its original content (excluding Content provided by
+          you or other users), features and functionality are and will remain
+          the exclusive property of Beluga and its licensors. The Service is
+          protected by copyright, trademark, and other laws of both the Country
+          and foreign countries.
+        </p>
+        <p>
+          Our trademarks and trade dress may not be used in connection with any
+          product or service without the prior written consent of Beluga.
+        </p>
+        <p className='text-black text-3xl'>Your Feedback to Us</p>
+        <p>
+          You assign all rights, title and interest in any Feedback you provide
+          Beluga. If for any reason such assignment is ineffective, you agree to
+          grant Beluga a non-exclusive, perpetual, irrevocable, royalty free,
+          worldwide right and license to use, reproduce, disclose, sub-license,
+          distribute, modify and exploit such Feedback without restriction.
+        </p>
+        <p className='text-black text-3xl'>5. Termination</p>
+        <p>
+          We may terminate or suspend your Account immediately, without prior
+          notice or liability, for any reason whatsoever, including without
+          limitation if you breach these Terms and Conditions. Upon termination,
+          your right to use the Service will cease immediately. If you wish to
+          terminate your Account, you may simply discontinue using the Service.
+        </p>
+        <p className='text-black text-3xl'>6. Limitation of Liability</p>
+        <p>
+          We are not responsible for any loss of items, content, boosts, tokens,
+          currency, gamepass, ability loss due to compromise of account,
+          scamming, or unfair trade between another user. In some cases, Beluga
+          will attempt to fix any loss of items for any reason given. However,
+          we are responsible for any loss of items due to data resets, in-game
+          bugs/glitches, or Roblox data errors. Proof must be provided to our
+          support team in order for us to determine how to compensate the user.
         </p>
       </div>
       <Footer />
